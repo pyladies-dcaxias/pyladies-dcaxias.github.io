afb12be617fc167546b314e97a07e4e847817c71
Paula Grangeiro <pgrangeiro.dev@gmail.com>
2015-11-16 17:37:06 -0200

    Corrigindo SITEURL

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Paula Grangeiro'
 SITENAME = u'PyLadies Duque de Caxias'
-SITEURL = ''
+SITEURL = 'http://duquedecaxias.pyladies.com'
 
 PATH = 'content'
 
